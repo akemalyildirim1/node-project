1c5218a87a6c5c6c004b8f89988afe8febee7f1b
akemalyildirim1 <akemalyildirim1@gmail.com>
2023-12-24 23:39:18 +0300

    test(setup): add setup and teardown files for testing

---
+++ test/setup/teardown.js
@@ -0,0 +1,13 @@
+import {connectMongoDB} from "../../database/mongo.js";
+import Countries from "../../models/countries.js";
+import config from "../test.config.js";
+
+const teardown = async () => {
+    console.log("Tearing down test data...");
+    await connectMongoDB(config.mongoDBUri);
+    await Countries.deleteMany({});
+    console.log("Test data torn down.")
+    process.exit(0);
+};
+
+teardown();
