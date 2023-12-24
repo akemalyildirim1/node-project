140569dc79ddd9d6565ad8c3a1b96acffc17f9cc
akemalyildirim1 <akemalyildirim1@gmail.com>
2023-12-24 23:55:47 +0300

    fix(test/setup): fix path of countries model

--- test/setup/setup.js
+++ test/setup/setup.js
@@ -1,6 +1,6 @@
 import {connectMongoDB} from "../../database/mongo.js";
 
-import Countries from "../../models/countries.js";
+import Countries from "../../models/Countries.js";
 import config from "../test.config.js";
 
 const setup = async () => {
