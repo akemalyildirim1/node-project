a00638f2fa66401cfbf39f38aabea91ab8d9f9cd
akemalyildirim1 <akemalyildirim1@gmail.com>
2023-12-24 23:39:18 +0300

    test(setup): add setup and teardown files for testing

---
+++ test/setup/setup.js
@@ -0,0 +1,28 @@
+import {connectMongoDB} from "../../database/mongo.js";
+
+import Countries from "../../models/countries.js";
+import config from "../test.config.js";
+
+const setup = async () => {
+    console.log("Setting up test data...");
+    await connectMongoDB(config.mongoDBUri);
+
+    const countries = [];
+
+    for (const {region, count} of [{region: 'europe', count: 15}, {region: 'apac', count: 8}, {
+        region: 'america',
+        count: 45
+    }]) {
+        for (let i = 0; i < count; i++) {
+            countries.push({name: `country${region}${i}`, region});
+        }
+    }
+
+    await Countries.insertMany(
+        countries
+    );
+    console.log("Test data set up.")
+    process.exit(0);
+};
+
+setup();
