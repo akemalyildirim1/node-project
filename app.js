0e48eb86af23b5d42d782e59e4404dedcf397d84
akemalyildirim1 <akemalyildirim1@gmail.com>
2023-12-24 23:44:44 +0300

    feat(app.js): add main application file
    The express application is started and two routers are added to the application. Also, the app is started after connecting to the database.

---
+++ app.js
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+import express from 'express';
+
+import {connectMongoDB} from "./database/mongo.js";
+import countryRouter from "./routes/countries-routes.js";
+import salesrepRouter from "./routes/salesrep-routes.js";
+
+dotenv.config();
+
+export const app = express();
+
+app.use(express.json());
+app.use("/countries", countryRouter);
+app.use("/salesrep", salesrepRouter);
+
+const mongoURI = process.env.MONGO_URI;
+
+connectMongoDB(
+    mongoURI
+).then(() => {
+        app.listen(3000, () => {
+                console.log("Server is running on port 3000");
+            }
+        )
+    }
+).catch((err) => {
+        console.log(err)
+        console.log("Connection failed");
+    }
+)
+
